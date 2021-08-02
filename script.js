78f1744d72b2f8bf0a835ec9064d6ff32d36dc78
Tom Dirinck <tom.dirinck@gmail.com>
2021-08-02 11:49:48 +0200

    Added carousel functionality.

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-Array.from(document.querySelectorAll(".letter")).forEach(el => {
-   el.innerText = randomLetter();
+Array.from(document.querySelectorAll('.letter')).forEach((el) => {
+  el.innerText = randomLetter();
 });
 
-function randomLetter(){
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    return alphabet[Math.floor(Math.random() * alphabet.length)]
+function randomLetter() {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  return alphabet[Math.floor(Math.random() * alphabet.length)];
 }
+
+function* generator() {
+  while (true) yield* [0, 1, 2];
+}
+const gen = generator();
+
+const showSlides = () => {
+  const slides = document.getElementsByClassName('slides');
+  let counter = gen.next().value;
+  for (let slide of slides) {
+    slide.style.display = 'none';
+  }
+  slides[counter].style.display = 'block';
+  setTimeout(() => {
+    showSlides();
+  }, 3000);
+};
+
+showSlides();
